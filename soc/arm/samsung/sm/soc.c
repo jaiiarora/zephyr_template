a82388fa584fdc0a339c27d605e46a944c6d404a
Jai Arora <infolinesoni@gmail.com>
2021-11-15 15:30:41 +0530

    soc.c updated

--- soc/arm/samsung/sm/soc.c
+++ soc/arm/samsung/sm/soc.c
@@ -19,22 +19,46 @@
 	#include <soc.h>
 	#include <kernel.h>
 
+
+	/*
+	* Setup the various clocks on soc
+	* According to the datasheet.
+	*/
+
+	static ALWAYS_INLINE void clock_init(void)
+	{
+		
+	}
+
+
 	/**
 	 * @brief Perform basic hardware initialization at boot.
-	 *
-	 * @return 0
+	 * Needs to run at the very beginning.
+	 * So, init property has to be 0 (zero).
+	 *@return 0
 	 */
 	static int sm_soc_init(const struct device *arg)
 	{
+		u32_t key;
 		ARG_UNUSED(arg);
+
+		key=irq_lock();
+
+		/* Setup the master clocks */
+		clock_init();
+
+		/* Disable watchdog timers if not used by the System */
+
 
 		/*
 		 * Install default handler that simply resets the CPU
 		 * if configured in the kernel, NOP otherwise.
 		 */
+
 		NMI_INIT();
 
+		irq_unlock(key);
 		return 0;
 	}
 
-	SYS_INIT(sm_soc_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
+	SYS_INIT(sm_soc_init, PRE_KERNEL_1, 0);
